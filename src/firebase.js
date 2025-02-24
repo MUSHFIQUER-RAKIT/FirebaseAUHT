1a2d23e908bf8ae9d34ab8637dabdcb35cf814a0
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2025-02-24 16:48:28 +0600

    Scaffolding

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0om_RHb2CRJoq_3C40IsSRbcPgTf-_PA",
+  authDomain: "readbooks-ac77f.firebaseapp.com",
+  projectId: "readbooks-ac77f",
+  storageBucket: "readbooks-ac77f.firebasestorage.app",
+  messagingSenderId: "1045260559855",
+  appId: "1:1045260559855:web:e77baf198e51ca4967021a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
